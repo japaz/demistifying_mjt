2e0e51775d10e81cca30c6ef26f64309b9d243db
Alberto Paz <alberto.paz@vilt.es>
2016-02-17 20:01:12 +0100

    Added evaluated and rejected routes and pages

---
+++ src/components/EvaluatedSubmissionsPage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import SubmissionsList from './SubmissionsList';
+import SubmissionsListPage from './SubmissionsListPage';
+import { select } from './SubmissionsListPage';
+import { connect } from 'react-redux';
+import { fetchSubmissionsList } from '../actions_creators/SubmissionsListActionsCreator';
+
+class PendingSubmissionsPage extends SubmissionsListPage {
+  static fetchData(dispatch, params) {
+    return dispatch(fetchSubmissionsList('evaluated'));
+  }
+
+  componentWillMount() {
+    PendingSubmissionsPage.fetchData(this.props.dispatch);
+  }
+
+  render() {
+    return (
+      <SubmissionsList attributes={this.attributes()}
+        submissions={this.props.submissions} />
+    );
+  }
+};
+
+export default connect(select)(PendingSubmissionsPage);
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Route } from 'react-router';
 import Main from './components/Main';
 import PendingSubmissionsPage from './components/PendingSubmissionsPage';
+import EvaluatedSubmissionsPage from './components/EvaluatedSubmissionsPage';
+import RejectedSubmissionsPage from './components/RejectedSubmissionsPage';
 import SubmissionFormPage from './components/SubmissionFormPage';
 import SubmissionPage from './components/SubmissionPage';
 
 export default (
   <Route path="/" component={Main}>
     <Route path="pending" component={PendingSubmissionsPage} />
+    <Route path="evaluated" component={EvaluatedSubmissionsPage} />
+    <Route path="rejected" component={RejectedSubmissionsPage} />
     <Route path="submissions/new" component={SubmissionFormPage} />
     <Route path="submissions/:id" component={SubmissionPage} />
   </Route>
