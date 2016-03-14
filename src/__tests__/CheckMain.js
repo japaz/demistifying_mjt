49c430454d9aef4ef304c51ac9ca87495b350028
Alberto Paz <alberto.paz@vilt.es>
2016-03-14 13:09:52 +0100

    Added simple test with jest

--- src/__tests__/CheckMain.js
+++ src/__tests__/CheckMain.js
@@ -7,9 +7,20 @@ import TestUtils from 'react-addons-test-utils';
 const Main = require('../components/Main').default;
 
 describe('Main', () => {
-	it('render a menu', () => {
-		var main = TestUtils.renderIntoDocument(
+	it('should exists', function() {
+    	// Render into document
+		const main = TestUtils.renderIntoDocument(
 			<Main children="" />
 		);
+    	expect(TestUtils.isCompositeComponent(main)).toBeTruthy();
+  	});
+
+	it('renders a menu', () => {
+		const main = TestUtils.renderIntoDocument(
+			<Main children="" />
+		);
+
+		const menu = TestUtils.findRenderedDOMComponentWithClass(main, "menu");
+		expect(menu).toBeDefined();
 	});
 });
